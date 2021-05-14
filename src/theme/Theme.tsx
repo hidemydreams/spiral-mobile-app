ecfb793f3bb3579c9b006bf5c1576ee37b81ab85
hidemydreams <naza.archakov222@gmail.com>
2021-05-15 02:15:02 +0300

    refactor: configured theme

--- src/theme/Theme.tsx
+++ src/theme/Theme.tsx
@@ -3,18 +3,19 @@ const theme = {
     primary: 'rgb(215, 51, 116)',
     textColor: 'rgb(41,41,41)',
   },
-  typography: {
-    fontFamily: 'Sf Pro Rounded',
-    thin: '200',
-    regular: '400',
-    bold: '500',
-  },
-  layout: {
-    container: {
-      paddingLeft: 10,
-      paddingRight: 10,
-    },
-  },
 };
 
 export default theme;
+
+// typography: {
+//     fontFamily: 'Sf Pro Rounded',
+//     thin: '200',
+//     regular: '400',
+//     bold: '500',
+//   },
+//   layout: {
+//     container: {
+//       paddingLeft: 10,
+//       paddingRight: 10,
+//     },
+//   },
