1cb6260c28082d2e237f487b420ff0c1e955f54d
hidemydreams <naza.archakov222@gmail.com>
2021-05-21 19:03:59 +0300

    feat: add mock data for flarlist

---
+++ src/data/data.ts
@@ -0,0 +1,32 @@
+export const GIVING_CARD_DATA = [
+  {
+    id: 1,
+    avatar: require('../assets/images/avatar.png'),
+    title: 'Your Giving Impact',
+    place: 'St Jude',
+    timestamp: '4 hrs ago',
+    image: require('../assets/images/rectangle2.png'),
+    description: `Danny, Your donation helped 5 amazing kids get much needed cancer
+        surgery, thanks for being amazing`,
+  },
+  {
+    id: 2,
+    avatar: require('../assets/images/avatar.png'),
+    title: 'Your Giving Impact',
+    place: 'St Jude',
+    timestamp: '4 hrs ago',
+    image: require('../assets/images/rectangle2.png'),
+    description: `Danny, Your donation helped 5 amazing kids get much needed cancer
+        surgery, thanks for being amazing`,
+  },
+  {
+    id: 3,
+    avatar: require('../assets/images/avatar.png'),
+    title: 'Your Giving Impact',
+    place: 'St Jude',
+    timestamp: '4 hrs ago',
+    image: require('../assets/images/rectangle2.png'),
+    description: `Danny, Your donation helped 5 amazing kids get much needed cancer
+        surgery, thanks for being amazing`,
+  },
+];
