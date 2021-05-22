524d4f2cc2caa4deabd9400ec0b3d34990a3903b
hidemydreams <naza.archakov222@gmail.com>
2021-05-23 02:30:33 +0300

    feat: add basic layout for profile screen

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { Input, Button } from 'react-native-elements';
+import { DarkText, LightText } from '../../components/styledComponents';
 
 function Profile() {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <View style={styles.profileInfo}>
+        <Image
+          style={styles.avatar}
+          source={require('../../assets/images/avatar_image.webp')}
+        />
+        <DarkText style={styles.profileName}>Full Name</DarkText>
+        <DarkText style={styles.profileDate}>05.15.1999</DarkText>
+      </View>
+      <Button title={<LightText>Edit profile</LightText>} />
+      <View style={styles.buttonContainer}>
+        <Button title={<LightText>Apply Changes</LightText>} />
+        <Button title={<LightText>Cancel</LightText>} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profileInfo: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  avatar: {
+    width: 150,
+    height: 150,
+    marginBottom: 60,
+  },
+  profileName: {
+    marginBottom: 20,
+    fontWeight: '500',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginBottom: 50,
+  },
+  profileDate: {},
+});
 
 export default Profile;
