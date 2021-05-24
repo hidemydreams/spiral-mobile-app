3399b225a23cf479203402c13ae924d49342d009
hidemydreams <naza.archakov222@gmail.com>
2021-05-24 19:19:20 +0300

    refactor: remove unused params

--- src/redux/actions/loginActions.ts
+++ src/redux/actions/loginActions.ts
@@ -6,7 +6,7 @@ const login = () => {
   };
 };
 
-const loginSuccess = (username: string, password: string) => {
+const loginSuccess = () => {
   return {
     type: auth.LOGIN_SUCCESS,
     payload: 'ANYJWTTOKEN',
@@ -30,8 +30,8 @@ export const signIn = (username: string, password: string) => {
   return function (dispatch: any) {
     return new Promise((resolve, reject) => {
       dispatch(login());
-      const re = /^[\w.+\-]+@itechart-group\.com$/;
-      const isValid = re.test(username) && password === 'admin';
+      const reg = /^[\w.+\-]+@itechart-group\.com$/;
+      const isValid = reg.test(username) && password === 'admin';
       setTimeout(() => {
         isValid ? resolve('Successfull Login') : reject();
       }, 1000);
