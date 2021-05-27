e7175e9fa314dd67ce7f469aa9a75349886d384a
hidemydreams <naza.archakov222@gmail.com>
2021-05-27 20:26:16 +0300

    feat: add splash screen and masked view packages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,15 +7,20 @@
  *
  * @format
  */
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppNavigationContainer from '../../navigations';
 import { ThemeProvider } from 'react-native-elements';
 import theme from '../../theme/Theme';
 import { Provider } from 'react-redux';
 import { persistedStore, store } from '../../redux/store';
 import { PersistGate } from 'redux-persist/lib/integration/react';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate persistor={persistedStore}>
